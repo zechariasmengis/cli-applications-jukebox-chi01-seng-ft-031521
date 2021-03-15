5c147d0b76183456cfbd4285e4c4ba082f76e506
Zecharias Mengis <zmengis@gmail.com>
2021-03-15 02:46:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.strip
   
-  if 1 <= user_response <= 9
+  if 1 <= user_response.to_i <= 9
     songs.each_with_index do |song, index|
       if user_response == index
         selected_song = user_response
