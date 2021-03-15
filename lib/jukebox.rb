8383fbecda54e5cdf7ba789e5ef4e49b9ed483e9
Zecharias Mengis <zmengis@gmail.com>
2021-03-15 02:52:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,8 +33,8 @@ def play(songs)
   
   if user_response.to_i <= 9 && user_response.to_i >= 1
     songs.each_with_index do |song, index|
-      if user_response == index
-        selected_song = user_response
+      if user_response == (index + 1)
+        puts "Playing #{song}"
       end
     end
   end
