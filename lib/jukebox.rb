f0e90c29514e3c5a0d84e4a3f89b7245a304ad95
Zecharias Mengis <zmengis@gmail.com>
2021-03-15 03:14:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,7 +13,7 @@ songs = [
 ]
 
 def help()
-  puts "Please enter a command:"
+  puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
